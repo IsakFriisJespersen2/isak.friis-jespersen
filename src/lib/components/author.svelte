24ba47379a615e2d5b9610f346b50a262ae8bac2
Isak Friis-Jespersen <isak.friis-jespersen@booli.se>
2023-09-13 16:31:45 +0200

    Refactored author

--- src/lib/components/author.svelte
+++ src/lib/components/author.svelte
@@ -1,31 +1,32 @@
 <script lang="ts">
-	import Programming from "$lib/icons/programming.svelte";
+	import Programming from '$lib/icons/programming.svelte';
 	let author = '';
 	let title = '';
 
-	const print = async (str: string) => {
-		for (let i = 0; i < str.length; i++) {
-			const char = str[i];
-			author += char;
-			await new Promise((resolve) => setTimeout(resolve, 70));
-		}
-	};
-	const print2 = async (str: string) => {
-		for (let i = 0; i < str.length; i++) {
-			const char = str[i];
-			title += char;
-			await new Promise((resolve) => setTimeout(resolve, 70));
-		}
-	};
+	const addCharToDom = async (str: string, callback: Function) =>
+		new Promise(async (resolve) => {
+			function* charGenerator(str: string) {
+				for (let i = 0; i < str.length; i++) {
+					yield new Promise((resolve) => {
+						setTimeout(() => resolve(str[i]), 60);
+					});
+				}
+			}
+			for await (let char of charGenerator(str)) {
+				callback(char);
+			}
+			resolve('ok');
+		});
 
-	print('Meet Isak Friis-Jespersen');
-	print2('a software engineer');
+	const start = async () => {
+		await addCharToDom('Meet Isak Friis-Jespersen', (char: string) => (author += char));
+		addCharToDom('a software engineer', (char: string) => (title += char));
+	};
+	start();
 </script>
 
-<div class="">
-	<h3 class="text-5xl text-center">
-		{author}<span class="animate-pulse font-bold text-orange-600 text-5xl">|</span>
-	</h3>
-	<h5 class="text-3xl w-full flex justify-end">{title}</h5>
-</div>
+<h3 class="text-5xl text-center">
+	{author}<span class="animate-pulse font-bold text-orange-600 text-5xl">|</span>
+</h3>
+<h5 class="text-3xl w-full flex justify-end">&nbsp;{title}</h5>
 <Programming className="h-96" />

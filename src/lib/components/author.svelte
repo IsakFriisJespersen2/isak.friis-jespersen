4ba37a9f379c7e857896e1f25d98df76adced817
Isak Friis-Jespersen <isak.friis-jespersen@booli.se>
2023-09-13 16:44:25 +0200

    Added move cursor logic

--- src/lib/components/author.svelte
+++ src/lib/components/author.svelte
@@ -2,6 +2,7 @@
 	import Programming from '$lib/icons/programming.svelte';
 	let author = '';
 	let title = '';
+	let cursorPosition = 'author';
 
 	const addCharToDom = async (str: string, callback: Function) =>
 		new Promise(async (resolve) => {
@@ -20,13 +21,20 @@
 
 	const start = async () => {
 		await addCharToDom('Meet Isak Friis-Jespersen', (char: string) => (author += char));
+		cursorPosition = 'title';
 		addCharToDom('a software engineer', (char: string) => (title += char));
 	};
 	start();
 </script>
 
 <h3 class="text-5xl text-center">
-	{author}<span class="animate-pulse font-bold text-orange-600 text-5xl">|</span>
+	{author}<span class="animate-pulse font-bold text-orange-600 text-5xl {cursorPosition !== 'author' && 'hidden'}"
+		>|</span
+	>
 </h3>
-<h5 class="text-3xl w-full flex justify-end">&nbsp;{title}</h5>
+<h5 class="text-3xl w-full flex justify-end">
+	&nbsp;{title}<span class="animate-pulse font-bold text-orange-600 text-3xl {cursorPosition !== 'title' && 'hidden'}"
+		>|</span
+	>
+</h5>
 <Programming className="h-96" />

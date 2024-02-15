cd649c90006085dd19b6282edbd365bed81fb1c7
Isak Friis-Jespersen <isak.friis-jespersen@booli.se>
2024-02-15 08:46:54 +0100

    fixed responsive about

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -4,19 +4,21 @@
 
 <div class="flex flex-col items-center gap-6">
 	<div class="text-3xl">+ About +</div>
-	<div class="rounded-full p-10 w-[40rem] h-[40rem] text-center bg-[#18262b]">
-		<div class="w-full h-full flex flex-col items-center gap-6 text-lg">
-		<img alt="My profile" src={profile} class="w-44 h-44 rounded-full"/>
+	<div
+		class="rounded-full p-10 desktop:w-[40rem] desktop:h-[40rem] tablet:w-[36rem] tablet:h-[36rem] w-[28rem] h-[28rem] text-center bg-[#18262b]"
+	>
+		<div class="w-full h-full flex flex-col items-center tablet:gap-4 gap-3 desktop:text-lg tablet:text-md text-sm">
+			<img alt="My profile" src={profile} class="desktop:w-40 desktop:h-40 tablet:w-32 tablet:h-32 w-28 h-28 rounded-full tablet:mb-6" />
 			<div>
 				Welcome to my site! I am a software developer based in Stockholm, Sweden, who is passionate about modern
 				technologies and creative ways of solving problems
 			</div>
-			<div>. . . .</div>
+			<div class="text-3xl text-orange-600">. . . .</div>
 			<div>
 				In addition to fullstack web development I love experimenting with, k8s technologies hosting stuff from my
 				raspberry pi.
 			</div>
-			<div>. . .</div>
+			<div class="text-3xl text-orange-600">. . .</div>
 		</div>
 	</div>
 </div>

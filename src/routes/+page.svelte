c866cfd29af40b857fd49f4f54a067724658b306
Isak Friis-Jespersen <isak.friis-jespersen@booli.se>
2024-02-15 07:43:56 +0100

    Added section border

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,18 +6,22 @@
 </script>
 
 <main class="bg-[#111d21] text-cyan-50">
-	<div class="w-full flex flex-col items-center gap-y-60">
-		<section class="h-5/6 mt-20">
+	<div class="w-full flex flex-col items-center gap-y-24">
+		<section class="h-5/6 mt-16">
 			<Author />
 		</section>
+		<div class="w-full border border-dashed"/>
 		<section class="h-5/6 w-full">
 			<About />
 		</section>
+		<div class="w-full border border-dashed"/>
 		<section>
 			<Experience />
 		</section>
+		<div class="w-full border border-dashed"/>
 		<section>
 			<Skills />
 		</section>
+		<div class="w-full border border-dashed"/>
 	</div>
 </main>
